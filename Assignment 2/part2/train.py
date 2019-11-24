104fdfbe11fad3c7b600e322db83662f2df64d44
paulltk <paultenkaate@outlook.com>
2019-11-24 14:14:25 +0100

    Update train.py

--- Assignment 2/part2/train.py
+++ Assignment 2/part2/train.py
@@ -55,8 +55,9 @@ def train(config):
         print(batch_inputs.size())
         print(batch_targets)
         print(batch_targets.size())
-        test = dataset.convert_to_string(batch_inputs)
-        print(test)
+        for sen in batch_inputs:
+            test = dataset.convert_to_string(sen)
+            print(test)
         
 
         break
