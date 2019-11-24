9a3319f6d5aa2786affda4503b6daee4b74eb4a5
paulltk <paultenkaate@outlook.com>
2019-11-24 15:29:40 +0100

    Update model.py

--- Assignment 2/part2/model.py
+++ Assignment 2/part2/model.py
@@ -26,11 +26,11 @@ class TextGenerationModel(nn.Module):
                  lstm_num_hidden=256, lstm_num_layers=2, device='cuda:0'):
 
         super(TextGenerationModel, self).__init__()
-        super.batch_size = batch_size 
-        super.seq_length = seq_length
-        super.vocabulary_size = vocabulary_size 
-        super.lstm_num_hidden = lstm_num_hidden
-        super.lstm_num_layers = lstm_num_layers
+        self.batch_size = batch_size 
+        self.seq_length = seq_length
+        self.vocabulary_size = vocabulary_size 
+        self.lstm_num_hidden = lstm_num_hidden
+        self.lstm_num_layers = lstm_num_layers
         
         layers = []
         layers.append(nn.LSTM(input_size=self.vocabulary_size, hidden_size = self.lstm_num_hidden, num_layers = self.lstm_num_layers))
