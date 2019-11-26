cdcbd26c5bade0192936ba9af0f73b7ab31dffa5
paulltk <paultenkaate@outlook.com>
2019-11-26 11:54:34 +0100

    Update train.py

--- Assignment 2/part2/train.py
+++ Assignment 2/part2/train.py
@@ -81,6 +81,7 @@ def train(config):
         accuracy = acc(out, batch_targets)
 
         optimizer.zero_grad()
+
         loss.backward()
         
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
@@ -127,11 +128,11 @@ if __name__ == "__main__":
     # parser.add_argument('--txt_file', type=str, default="assets/book_EN_democracy_in_the_US.txt", help="Path to a .txt file to train on")
     parser.add_argument('--seq_length', type=int, default=30, help='Length of an input sequence')
     parser.add_argument('--lstm_num_hidden', type=int, default=128, help='Number of hidden units in the LSTM')
-    parser.add_argument('--lstm_num_layers', type=int, default=2, help='Number of LSTM layers in the model')
+    parser.add_argument('--lstm_num_layers', type=int, default=3, help='Number of LSTM layers in the model')
 
     # Training params
-    parser.add_argument('--batch_size', type=int, default=512, help='Number of examples to process in a batch')
-    parser.add_argument('--learning_rate', type=float, default=1e-2, help='Learning rate')
+    parser.add_argument('--batch_size', type=int, default=128, help='Number of examples to process in a batch')
+    parser.add_argument('--learning_rate', type=float, default=2e-3, help='Learning rate')
 
     # It is not necessary to implement the following three params, but it may help training.
     parser.add_argument('--learning_rate_decay', type=float, default=0.96, help='Learning rate decay fraction')
