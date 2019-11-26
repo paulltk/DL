9cfaf85c26c6c75ea687161be5660f6c86f9d074
paulltk <paultenkaate@outlook.com>
2019-11-26 11:16:03 +0100

    Update train.py

--- Assignment 2/part2/train.py
+++ Assignment 2/part2/train.py
@@ -126,8 +126,8 @@ if __name__ == "__main__":
     parser.add_argument('--lstm_num_layers', type=int, default=2, help='Number of LSTM layers in the model')
 
     # Training params
-    parser.add_argument('--batch_size', type=int, default=64, help='Number of examples to process in a batch')
-    parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--batch_size', type=int, default=512, help='Number of examples to process in a batch')
+    parser.add_argument('--learning_rate', type=float, default=2e-3, help='Learning rate')
 
     # It is not necessary to implement the following three params, but it may help training.
     parser.add_argument('--learning_rate_decay', type=float, default=0.96, help='Learning rate decay fraction')
